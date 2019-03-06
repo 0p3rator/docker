43b37f227df1598ce20f2cde08aa3bca0cec1d35
Sun Zhongsheng <1294477941@qq.com>
2019-03-06 18:08:48 +0800

    add usernamesppace and network_namespace code

---
+++ network_ns.go
@@ -0,0 +1,43 @@
+package main
+
+// 	1, "ifconfig" in system shell
+//      2, "ifconfig" in "sh" shell
+
+//      3, You will find nothing result in "sh" shell, so we can conclude that network devices have been seperated already.
+import (
+	"log"
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+func main() {
+	cmd := exec.Command("sh")
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWUSER | syscall.CLONE_NEWNET,
+		// The code in the book can not work, beacuse after linux kernel 3.19, user namespace has been changed, so I make some improvement
+		UidMappings: []syscall.SysProcIDMap{
+			{
+				ContainerID: 0,
+				HostID:      0,
+				Size:        1,
+			},
+		},
+		GidMappings: []syscall.SysProcIDMap{
+			{
+				ContainerID: 0,
+				HostID:      0,
+				Size:        1,
+			},
+		},
+	}
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
+	os.Exit(-1)
+}
